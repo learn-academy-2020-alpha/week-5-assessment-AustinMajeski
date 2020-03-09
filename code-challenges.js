b1e1d2289638eff634c00883a0f03ba27e995131
HOME-BASE\natbl <natblarg123@gmail.com>
2020-03-08 23:23:04 -0700

    all done

--- code-challenges.js
+++ code-challenges.js
@@ -1,36 +1,315 @@
+
 // ASSESSMENT 5: JavaScript Coding Practical Questions
 
-// --------------------1) Create a function that takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.
+/**********************/
+/*   Austin Majeski   */
+/**********************/
+
+
+
+
+    // -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*-
+
+
+    //               Custom Logging To Console
+
+
+    // -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*-
+
+
+class CustomLog{
+
+    static _init_SET_UP = CustomLog._init_SET_UP_Function()
+
+    static _init_SET_UP_Function() {
+
+        // ============= ============= ============= ============= ============= ============= =============
+
+        //  #  Giving the ' call_custom_log ' method to the Array type
+
+        Array.prototype.get_custom_log_format = function() {
+            let stopping_index = this.length - 1
+            let output = "[ "
+            this.forEach( (v,i)  =>  i < stopping_index ? output += `${v}, ` : output += v )
+            return( output + " ]" )
+        }
+
+        //  #  Giving the ' emphasize ' method to Strings
+
+        String.prototype.emphasize = function(  spacing = 0, color = CustomLog.Use.GREEN_TEXT  ) {
+            let local_space = (  spacing > 0 ? " ".repeat(spacing) : ""  )
+            return( `${CustomLog.Use.BLACK_BACKGROUND}${color}${local_space}${this}${local_space}${CustomLog.Use.FORMAT_RESET}` )
+        }
+
+        // ============= ============= ============= ============= ============= ============= =============
+
+        return 'Done'
+    }
+
+
+    // - : -- : --- : -- : - : -- : --- : -- : - : -- : --- : -- : - : -- : --- : -- : - : -- : --- : -- : - :
+    // = : == : === : == : = : == : === : == : = : == : === : == : = : == : === : == : = : == : === : == : = :
+    // - : -- : --- : -- : - : -- : --- : -- : - : -- : --- : -- : - : -- : --- : -- : - : -- : --- : -- : - :
+
+
+    static Values = {
+
+        // # Used for spacing
+        LENGTH:	        70,	//55
+        INDENT_SIZE:    3,
+
+        // # Characters
+        ESC:	String.fromCharCode(27)
+
+    }
+
+    // -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+    static Colors = {
+        white: 15,
+        black: 0,
+        green: 10,
+        pink: 201,
+        deep_blue: 217
+    }
+
+    // -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+    static Use = {
+
+        // # Single Definitions
+
+        init_push_down:     console.log( "\n\n" ),
+        SPACE:              " ".repeat( CustomLog.Values.INDENT_SIZE*2 + 1 ),
+        INDENT:	            " ".repeat( CustomLog.Values.INDENT_SIZE ),
+
+        // # Enhanced Definitions
+
+        SOLID_LINE:	        "_".repeat( CustomLog.Values.LENGTH ),
+        BLANK_LINE:	        " ".repeat( CustomLog.Values.LENGTH ),
+
+        // # Background Colors
+
+        WHITE_BACKGROUND:	CustomLog.Values.ESC+"[48;5;15m",
+        BLACK_BACKGROUND:	CustomLog.Values.ESC+"[48;5;0m",
+        BLUE_BACKGROUND:	CustomLog.Values.ESC+"[48;5;"+CustomLog.Colors.deep_blue+"m",
+
+        // # Foreground Colors
+
+        WHITE_TEXT:	        CustomLog.Values.ESC+"[38;5;15m",
+        GREEN_TEXT:	        CustomLog.Values.ESC+"[38;5;"+CustomLog.Colors.green+"m",
+        PINK_TEXT:	        CustomLog.Values.ESC+"[38;5;"+CustomLog.Colors.pink+"m",
+
+        // # Reset
+
+        FORMAT_RESET:	    CustomLog.Values.ESC+"[0m"	// => [ ! ]   Reset
+    }
+
+
+
+    // - : -- : --- : -- : - : -- : --- : -- : - : -- : --- : -- : - : -- : --- : -- : - : -- : --- : -- : - :
+    // = : == : === : == : = : == : === : == : = : == : === : == : = : == : === : == : = : == : === : == : = :
+    // - : -- : --- : -- : - : -- : --- : -- : - : -- : --- : -- : - : -- : --- : -- : - : -- : --- : -- : - :
+
+
+
+    //  #  Log Function  #  //
+    static log() {
+        console.log(
+			`\n\n${CustomLog.top_banner}\
+			\n\n${CustomLog.Use.SPACE}${arguments[0]}\
+			\n${CustomLog.Use.INDENT}${CustomLog.Use.SOLID_LINE}\n\n`
+		)
+        let Args = Array.from(arguments).splice(1)
+        Args.forEach( w => { console.log(  CustomLog.Use.SPACE + (
+		    typeof w.get_custom_log_format === 'undefined' ?
+                w : w.get_custom_log_format()
+            ))
+        })
+        console.log(  
+            `\n${CustomLog.Use.INDENT}${CustomLog.Use.SOLID_LINE}\
+            \n${CustomLog.Use.INDENT}${CustomLog.Use.SOLID_LINE}\n\n\n`  )
+    }
+
+
+    //  #  Simplifying  #  //
+    static get top_banner(){ return(
+        `${CustomLog.Use.INDENT}${CustomLog.Use.WHITE_BACKGROUND}${CustomLog.Use.BLANK_LINE}${CustomLog.Use.FORMAT_RESET}`
+    )}
+}
+
+
+
+// ============= ============= ============= ============= ============= ============= ============= ============= =============
+// ============= ============= ============= ============= ============= ============= ============= ============= =============
 
 
 
 
 
-var secretCodeWord1 = "lackadaisical"
-// Expected output: "l4ck4d41s1c4l"
-var secretCodeWord2 = "gobbledygook"
-// Expected output: "g0bbl3dyg00k"
-
-// --------------------2) Create a function that takes in an array and returns all the words that have the letter a in them.
 
 
+    // -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*-
 
 
+    //                   Assessment Start
 
-var arrayOfWords = ["Apple", "Banana", "Plum", "Cherry", "Kiwi", "Peach"]
-// Expected output: "Apple" "Banana" "Peach"
+
+    // -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*- -*-
 
 
 
 
 
-// --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is one pair and one three of a kind.
+// ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== -
+//  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --
+
+// --------------------1)   Create a function that takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.
+
+//  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --
+// ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== -
+
+
+var secretCodeWord1 = "lackadaisical"   // Expected output: "l4ck4d41s1c4l"
+var secretCodeWord2 = "gobbledygook"    // Expected output: "g0bbl3dyg00k"
+
+
+//  #  Giving the ' code_my_message ' method to the String type
+String.prototype.code_my_message = function() {
+    
+    let convert = {
+        'a' : '4',
+        'e' : '3',
+        'i' : '1',
+        'o' : '0'
+    }
+
+    return (  Array.from( this ).map( letter => typeof convert[letter] !== 'undefined' ? convert[letter] : letter ).join('')  )
+}
+
+
+
+// ============= ============= 
+
+CustomLog.log(  "Coded Message",
+    
+    `${secretCodeWord1}   =>   ${ secretCodeWord1.code_my_message().emphasize() }`,
+    `${secretCodeWord2}    =>   ${ secretCodeWord2.code_my_message().emphasize() }`
+)
+
+// ============= ============= 
 
 
 
 
 
-var hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-var hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
+
+// ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== -
+//  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --
+
+// --------------------2)   Create a function that takes in an array and returns all the words that have the letter 'a' in them.
+
+//  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --
+// ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== -
+
+
+var arrayOfWords = ["Apple", "Banana", "Plum", "Cherry", "Kiwi", "Peach"]   // Expected output: "Apple" "Banana" "Peach"
+
+
+//  #  Giving the ' get_items_with_letter ' method to the Array type
+Array.prototype.get_items_with_letter = function() {
+    
+    if( typeof arguments[0] !== 'string' ) { return this }
+    let letter_to_find = arguments[0].toLowerCase()
+
+    return this.filter(  item  =>  typeof item === 'string'  &&  item.toLowerCase().includes( letter_to_find )  )
+}
+
+
+
+// ============= ============= 
+
+CustomLog.log(  "All Words with the Letter 'A'",
+    
+    `Array Used   =>   ${arrayOfWords.get_custom_log_format()}`,
+    '',
+    `   =>   ${ arrayOfWords.get_items_with_letter( 'a' ).get_custom_log_format().emphasize() }`
+)
+
+// ============= ============= 
+
+
+
+
+
+
+// ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== -
+//  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --
+
+// --------------------3)   Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is one pair and one three of a kind.
+
+//  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --
+// ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== - ==== -
+
+
+var hand1 = [5, 5, 5, 3, 3] // Expected output: true
+var hand2 = [5, 5, 3, 3, 4] // Expected output: false
+
+var hand3 = [ 8, 8, 8, 8, 8 ] // Expected output: false
+var hand4 = [ 1, 1, 0, 1, 1 ] // Expected output: false
+
+
+
+Array.prototype.is_fullhouse = function() {
+    
+    if( this.length !== 5 ) { return false }
+
+    let found = (
+         this.reduce( (acc, curr) => {
+             if ( typeof acc[curr] === 'undefined' ) {
+                 acc[curr] = 1;
+             } else {
+                 acc[curr] += 1;
+             }
+
+             return acc;
+         }, {} )
+    )
+
+    return Object.keys(found).length === 2 ? 
+        (( Object.entries(found)[1][1] === 2 || Object.entries(found)[1][1] === 3 ) ? true : false)
+        : false
+   
+}
+
+
+
+// ============= ============= 
+
+CustomLog.log(  "Full House?",
+
+    `${hand1.get_custom_log_format()}   =>   ${ hand1.is_fullhouse().toString().emphasize(2) }${" ".emphasize()}`,
+    `${hand2.get_custom_log_format()}   =>   ${ hand2.is_fullhouse().toString().emphasize(2) }`,
+    '','',
+    'Another Example',
+    '---------------',
+    `${hand3.get_custom_log_format()}   =>   ${ hand2.is_fullhouse().toString().emphasize(2) }`,
+    `${hand4.get_custom_log_format()}   =>   ${ hand2.is_fullhouse().toString().emphasize(2) }`
+)
+
+// ============= ============= 
+
+
+
+
+
+
+
+
+
+
+
+
+// ============= ============= ============= ============= ============= ============= =============
+        console.log( "\n" )
+// EOF
--- interview-questions.md
+++ interview-questions.md
@@ -2,19 +2,57 @@
 
 Answer the following questions. First, without external resources. Challenge yourself to answer from memory. Then, research the question to expand on your answer. Even if you feel you have answered the question completely on your own there is always something more to learn.   
 
+
 1. Rails follows an MVC pattern. What does that mean?
 
   Your answer:
 
+	- Model, View, Controller
+	- That means the work is divided into three parts.
+		1.	The Model:   for the data.
+		2.	The View:     for the visible pages (website)
+		3.	The Controller:    what to do when certain actions are taken ( e.g.  goint to a specific extendtion of the website's URL )
+
+
+
   Researched answer:
 
+	MVC Pattern stands for Model-View-Controller Pattern. This pattern is used to separate application's concerns.
+
+	1.   Model 		- Represents an object or JAVA POJO* carrying data.  It can also have logic to update controller if its data changes.
+				> (  JAVA POJO )   =    " Plain Old Java Objects "
+					-  A straightforward type with no references to any particular frameworks.
+					-  A collection of properties and methods.
+					( PS: )	JAVA POJOs do not need to follow and real conventions.  
+						Many Java libraries support by default something called the ' JavaBean ' naming convention.
+					- - (  https://www.baeldung.com/java-pojo-class  )
+
+	2.   View 		- Represents the visualization of the data that model contains.
+
+	3.   Controller	- It controls the flow of data into the model object, and updates the View whenever the data changes.
+			> Acts on both the Model and View.
+			( It keeps view and model separate. )
+
+	
 
 
 2. What is a gem?
 
   Your answer:
 
+	- A dependency for your code to run.
+
+
   Researched answer:
+
+	- A Ruby ' gem ' is a self-contained program or library
+
+	> " RubyGems is a package manager for the Ruby programming language that provides a standard format for distributing Ruby programs and libraries (in a self-contained format called a "gem"), a tool designed to easily manage the installation of gems, and a server for distributing them.  "
+		- Wikipedia:
+		 (  https://en.wikipedia.org/wiki/RubyGems  )
+
+	(  https://guides.rubygems.org/what-is-a-gem/  )
+
 
 
 
@@ -22,31 +60,70 @@ Answer the following questions. First, without external resources. Challenge you
 
   Your answer:
 
+	- to prevent unwanted ' nils ', data outside of your desired range, or unique data, etc..
+
+
   Researched answer:
+
+	- Data validation is vital to ensure the data is clean, correct and useful.
+	
+ 	>  If the data isn't validated, any errors you get in your data are going to flow right through into the data warehouse.
+
+	>  A little bit of thought and planning upfront goes a long way to ensure your data is accurate and clean.
+	    ( And that you are capturing the right sort of data to run your analysis with. )
+
+	(  https://deltadna.com/blog/importance-data-validation/  )
+
 
 
 
 4. Describe a possible relationship between a model called Person and a model called Computer. Based on your description, which model would hold the foreign key?
 
   Your answer:
+	
+	< Relationship >   -   A Person (or many persons) would use a computer to look up things on the internet.
+
+	-  The Person would hold the foreign key, which would refer to the computer they use.
+	    A Computer can have multiple users (People/Persons).
+	    The Person, in this senario, would only use THAT one Computer.
+
 
   Researched answer:
 
+	- Wouldn't this be subjective to how you want to oganize your data?
+
+	[ I found this noteworthy ]  =  (  https://www.toptal.com/ruby-on-rails/top-10-mistakes-that-rails-programmers-make  )
+
+	
 
 
 5. What is object-relational mapping?
 
   Your answer:
 
+	- This is what we are doing when we do the ' has_many ' & ' belongs_to ' stuff with our models.
+	    Instead of having Objects 'contain' other Objects, we link them by how they are related to each other.
+
   Researched answer:
+
+	ORM
+
+	-  A programming technique for converting data between incompatible type systems using object-oriented programming languages.
+	(  I think this is where JSON 'objects' come in handy between Ruby and Javascript, right?  )
+
+
+	[ brief and informative ]   =   (  https://blog.bitsrc.io/what-is-an-orm-and-why-you-should-use-it-b2b6f75f5e2a  )
+	
+
 
 
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
-- RESTful routes
-- json
-- API
-- Endpoints
-- Strong params
+- RESTful routes	=	Routs to fall back on if the user goes to a link (still in your website) that does not link to a particular page?
+- json	=	A String representation of a Javascript Object; easily transferable to other languages, like Ruby.
+- API	=	An Interface designed by someone containin the code to do a particular task.   The programmer can use these to send something in, and get something out.  ( e.g.  and Encyrption API )
+- Endpoints	=	Where the servers communicate?
+- Strong params	=	Arguments in Ruby On Rails that follow strict rules?
+
